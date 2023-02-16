669ccc6676875c129c73280c145ccefe6a9989f8
Remix007 <104088605+Remix007@users.noreply.github.com>
2023-02-16 15:56:24 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import PhoneBookList from './PhoneBookList';
+import Form from './Form';
+import Filter from './Filter';
+
+class App extends Component {
+  state = {
+    phoneList: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+  formSubmitHendler = data => {
+    // console.log(data);
+    const { phoneList } = this.state;
+    const phoneBookListItem = {
+      id: nanoid(),
+      name: data.name,
+      number: data.number,
+    };
+    if (phoneList.find(item => item.name === phoneBookListItem.name)) {
+      return alert(`${phoneBookListItem.name}  is olrady in contacts`);
+    }
+    this.setState(prevState => ({
+      phoneList: [phoneBookListItem, ...prevState.phoneList],
+    }));
+  };
+
+  deletePhoneListItem = phoneListId => {
+    this.setState(prevState => ({
+      phoneList: prevState.phoneList.filter(
+        phoneListItem => phoneListItem.id !== phoneListId
+      ),
+    }));
+  };
+  changeFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+  getVisibleFilter = () => {
+    const { phoneList, filter } = this.state;
+
+    const normalizedFilter = filter.toLowerCase();
+
+    return phoneList.filter(item =>
+      item.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+
+    const filteredPhoneBookList = this.getVisibleFilter();
+
+    return (
+      <>
+        <Form onSubmit={this.formSubmitHendler} />
+
+        <Filter value={filter} onChange={this.changeFilter} />
+        <PhoneBookList
+          phoneList={filteredPhoneBookList}
+          onDeletePhoneListItem={this.deletePhoneListItem}
+        />
+      </>
+    );
+  }
+}
+export default App;
